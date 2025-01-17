92984f0fe0a9f73eabfcdc351aaef1aa8a7eaf11
lalit bhanot <lalitthebest@gmail.com>
2025-01-17 14:22:11 +0100

    Added Sql data injection.

--- databaseIntegrationtesting/src/main/java/com/lalitbhanot/springmvc/service/StudentAndGradeService.java
+++ databaseIntegrationtesting/src/main/java/com/lalitbhanot/springmvc/service/StudentAndGradeService.java
@@ -1,9 +1,35 @@
 package com.lalitbhanot.springmvc.service;
 
+import com.lalitbhanot.springmvc.models.CollegeStudent;
+import com.lalitbhanot.springmvc.repository.StudentDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
 public class StudentAndGradeService {
+
+    @Autowired
+    private StudentDao studentDao;
+
+    public void createStudent(String firstname, String lastname, String emailAddress) {
+        CollegeStudent student = new CollegeStudent(firstname, lastname, emailAddress);
+        student.setId(0);
+        studentDao.save(student);
+    }
+
+
+    public boolean checkStudentIdisNull(int id) {
+        Optional<CollegeStudent> student = studentDao.findById(id) ;
+        return  (student.isPresent()) ? true :false ;
+
+    }
+
+    public Iterable<CollegeStudent> getGradeBook() {
+        Iterable<CollegeStudent> studentsIterable = studentDao.findAll() ;
+        return  studentsIterable ;
+    }
 }
--- databaseIntegrationtesting/src/test/java/com/lalitbhanot/springmvc/StudentAndGradeServiceTest.java
+++ databaseIntegrationtesting/src/test/java/com/lalitbhanot/springmvc/StudentAndGradeServiceTest.java
@@ -1,21 +1,40 @@
 package com.lalitbhanot.springmvc;
 
+import com.lalitbhanot.springmvc.models.CollegeStudent;
+import com.lalitbhanot.springmvc.repository.StudentDao;
+import com.lalitbhanot.springmvc.service.StudentAndGradeService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestPropertySource("/application.properties")
 @SpringBootTest
 public class StudentAndGradeServiceTest {
-    @Test
-    public void createStudentService(){
-        @Autowired
-        private StudentAndGradeService studentService;
+    @Autowired
+    private StudentAndGradeService studentService;
 
-        @Autowired
-        private StudentDao studentDao;
+    @Autowired
+    private StudentDao studentDao;
 
+    @Autowired
+    private JdbcTemplate jdbctemplate ;
+
+    @BeforeEach
+    public void setupData()
+    {
+        jdbctemplate.execute("insert into student (firstName,lastName,email_address) values ('lalit','bhanot','lal@gmail.com') ;");
+    }
         @Test
         public void createStudentService() {
 
@@ -28,5 +47,32 @@ public class StudentAndGradeServiceTest {
             assertEquals("chad.darby@luv2code_school.com",
                     student.getEmailAddress(), "find by email");
         }
+
+    @Test
+    public void isStudentNullCheck(){
+assertTrue(studentService.checkStudentIdisNull(1));
     }
-}
+
+    @Sql("/insertdata.sql")
+    @Test
+    public void getGradeBookService (){
+        Iterable<CollegeStudent> studentsIterable = studentService.getGradeBook();
+        List<CollegeStudent> collegeStudentList = new ArrayList<>();
+        for(CollegeStudent collegeStudent:studentsIterable)
+        {
+            collegeStudentList.add(collegeStudent) ;
+        }
+        assertEquals(5,collegeStudentList.size());
+    }
+
+
+    @AfterEach
+    public  void AfterTranscation()
+    {
+        jdbctemplate.execute("DELETE FROM STUDENT");
+        jdbctemplate.execute("ALTER TABLE STUDENT ALTER COLUMN ID RESTART WITH 1");
+    }
+
+
+    }
+
